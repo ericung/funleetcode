82bd85150bbdde13efa969dd2a654d43a4255b7c
ericung <ungericw@tutanota.com>
2024-07-13 16:20:38 -0500

    climbing stairs

---
+++ c/000070ClimbingStairs.c
@@ -0,0 +1,32 @@
+int climbStairs(int n) {
+	// trivial cases
+	if (n < 3)
+	{
+		return n;
+	}
+
+	// prev and prevprev pointer variables
+	int prev = 1;
+	int prevprev = 2;
+
+	// loop invariant such that it starts at 3 and ends at n
+	for (int i = 3; i <= n; i++)
+	{
+		// temp variable such that it is the aggregate of the pointer variables
+		int temp = prev + prevprev;
+		prev = prevprev;
+		prevprev = temp;
+	}
+
+	// result variable
+	return prevprev;
+}
+
+/* On the analysis of the fibonacci sequence
+ * loop invariant such that i is in 3 to n
+ * 	prev = t_{n-2}
+ * 	prevprev = t_{n-1}
+ * 	while looping, temp = prevprev + prev
+ * 	or t_{n} = t_{n-1} + t_{n-2}
+ *
+ */
