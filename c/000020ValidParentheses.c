9a7bc5e279d5afa30a92fa20166ce24cf3f4c987
ericung <ungericw@tutanota.com>
2024-07-25 20:11:55 -0500

    struct *stack

--- c/000020ValidParentheses.c
+++ c/000020ValidParentheses.c
@@ -1,19 +1,22 @@
 // variables for operating on the stack
 // TODOLIST: turn into struct
 // TODOLIST: refactor
-char* stack;
-int SIZE;
-int pointer;
+struct Stack {
+	char* stack;
+	int SIZE;
+	int pointer;
+};
 
 // template from generic stack operations
-void push(char c);
-void pop();
-char top();
+void push(struct Stack *stack, char c);
+void pop(struct Stack *stack);
+char top(struct Stack *stack);
 
 bool isValid(char* s) {
-	stack = (char*)malloc(strlen(s)+1);
-	SIZE = strlen(s);
-	pointer = -1;
+	struct Stack *stack = malloc(sizeof(struct Stack));
+	stack->stack = (char*)malloc(strlen(s)+1);
+	stack->SIZE = strlen(s);
+	stack->pointer = -1;
 
 	// loop invariant is while i is between 0 and strlen(s) = n
 	for (int i = 0; i < strlen(s); i++)
@@ -21,14 +24,14 @@ bool isValid(char* s) {
 		// put s[k] in the stack
 		if ((s[i] == '(')||(s[i] == '{')||(s[i] == '['))
 		{
-			push(s[i]);
+			push(stack, s[i]);
 		}
 		// pop the stack if s[k] is a valid close
-		else if (((s[i] == ')')&&(top() == '('))
-			||((s[i] == '}')&&(top() == '{'))
-			||((s[i] == ']')&&(top() == '[')))
+		else if (((s[i] == ')')&&(top(stack) == '('))
+			||((s[i] == '}')&&(top(stack) == '{'))
+			||((s[i] == ']')&&(top(stack) == '[')))
 		{
-			pop();
+			pop(stack);
 		}
 		else
 		{
@@ -37,26 +40,26 @@ bool isValid(char* s) {
 	}
 
 	// if the pointer is -1 then return true otherwise return false
-	return pointer == -1 ? true : false;
+	return stack->pointer == -1 ? true : false;
 }
 
-void push(char c) {
-	if (pointer < SIZE) {
-		pointer++;
-		stack[pointer] = c;
+void push(struct Stack *stack, char c) {
+	if (stack->pointer < stack->SIZE) {
+		stack->pointer++;
+		stack->stack[stack->pointer] = c;
 	}
 }
 
-void pop() {
-	if (pointer >= 0) {
-		pointer--;
+void pop(struct Stack *stack) {
+	if (stack->pointer >= 0) {
+		stack->pointer--;
 	}
 }
 
-char top() {
-	if (pointer != -1)
+char top(struct Stack *stack) {
+	if (stack->pointer != -1)
 	{
-		return stack[pointer];
+		return stack->stack[stack->pointer];
 	}
 
 	return '\0';
