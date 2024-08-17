74c8bc5cead7649b2c58d132ae950adabbac3458
ericung <ungericw@tutanota.com>
2024-08-17 13:20:35 -0500

    001937MaximumNumberOfPointsWithCost.c

---
+++ c/001937MaximumNumberOfPointsWithCost.c
@@ -0,0 +1,34 @@
+long long maxPoints(int** points, int pointsSize, int* pointsColSize) {
+    long long dp[pointsSize][*pointsColSize];
+    long long max = 0;
+    long long value = 0;
+
+    for (int i = 0; i < pointsSize; i++) {
+        for(int j = 0; j < *pointsColSize; j++) {
+            dp[i][j] = (long long)points[i][j];
+        }
+    }
+
+    for (int i = 1; i < pointsSize; i++) {
+        value = 0;
+        for (int j = 0; j < *pointsColSize; j++) {
+            value = (value - 1) < dp[i-1][j] ? dp[i-1][j] : (value - 1);
+            dp[i][j] = value;
+        }
+
+        value = 0;
+        for (int j = *pointsColSize-1; j >= 0; j--) {
+            value = value - 1 < dp[i-1][j] ? dp[i-1][j] : value - 1;
+            dp[i][j] = value < dp[i][j] ? dp[i][j] : value;
+            dp[i][j] += points[i][j];
+        }
+    }
+
+    for (int j = 0; j < *pointsColSize; j++) {
+        if (dp[pointsSize - 1][j] > max) {
+            max = dp[pointsSize - 1][j];
+        }
+    }
+
+    return max;
+}
