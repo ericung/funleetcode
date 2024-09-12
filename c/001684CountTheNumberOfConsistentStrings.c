09ea1fc4d3cf0d2a483608b2b5c4c185d18034e1
ericung <ungericw@tutanota.com>
2024-09-12 16:05:51 -0500

    001684CountTheNumberOfConsistentStrings.c

---
+++ c/001684CountTheNumberOfConsistentStrings.c
@@ -0,0 +1,28 @@
+int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+	int *hash = malloc(sizeof(int*)*27);
+	int count = 0;
+
+	memset(hash, 0, sizeof(int*)*27);
+
+	for (int i = 0; i < strlen(allowed); i++) {
+		hash[allowed[i] - 'a']++;
+	}
+
+	for (int i = 0; i < wordsSize; i++) {
+		// loop invariant while hash[words[i][j] - 'a'] is not equal 0
+		// otherwise break
+		for (int j = 0; j < strlen(words[i]); j++) {
+			if (hash[words[i][j] - 'a'] == 0) {
+				break;
+			}
+
+			if (j == strlen(words[i]) - 1) {
+					count++;
+			}
+		}
+	}
+
+	return count;
+}
+
+// (S,w) = lambda * mu * w * gamma = lambda * mu * hash is an element of a A^27x27 * gamma
